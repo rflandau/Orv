c483011739ae126675a8a2d1e66ef4b38b7c26c2
rflandau <rflandau@pm.me>
2025-04-22 23:23:02 -0700

    add TODO; skeletonize STATUS

--- pkg/orv/endpoints.go
+++ pkg/orv/endpoints.go
@@ -15,8 +15,18 @@ type RespHello struct {
 	}
 }
 
+// Response for GET /status commands.
+// Returns the status of the current node.
+// TODO create handleSTATUS as a method on vk to return information about the status of the node
+// we can use this endpoint to query node info in our tests.
+type RespStatus struct {
+	Body struct {
+		Message string `json:"message" example:"Hello, world!" doc:"Greeting message"`
+	}
+}
+
 func handleHELLO(ctx context.Context, input *struct{}) (*RespHello, error) {
 	resp := &RespHello{}
-	//resp.Body.Message = "Hello, Orv!"
+	resp.Body.Message = "Hello, Orv!"
 	return resp, nil
 }
