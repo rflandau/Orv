7c423fcdc52c6adae12349329c1bcd56a87e1cfd
rflandau <rflandau@pm.me>
2025-04-24 08:58:11 -0700

    switch HELLO to POST

--- pkg/orv/endpoints.go
+++ pkg/orv/endpoints.go
@@ -20,11 +20,11 @@ const (
 // (hence no return value and no pointer parameter (yes, I know it is weird. Weird design decision on huma's part)).
 func (vk *VaultKeeper) buildRoutes() {
 	// Handle GET requests on /hello
-	huma.Get(vk.endpoint.api, HELLO, handleHELLO) // TODO switch handleHello to be a method on vk
+	huma.Post(vk.endpoint.api, HELLO, handleHELLO) // TODO switch handleHello to be a method on vk
 
 	// Handle GET requests on /status (using the more advanced .Register() method)
 	huma.Register(vk.endpoint.api, huma.Operation{
-		OperationID:   "get-status",
+		OperationID:   "status",
 		Method:        http.MethodGet,
 		Path:          STATUS,
 		Summary:       "", // TODO
@@ -39,7 +39,9 @@ func (vk *VaultKeeper) buildRoutes() {
 // Used by nodes to introduce themselves to the tree.
 // Theoretically, this could be a broadcast and the requester could then pick which HELLO response to follow up on
 type HelloReq struct {
-	Id uint64 `json:"id" required:"true" example:"718926735" doc:"unique identifier for this specific node"`
+	Body struct {
+		Id uint64 `json:"id" required:"true" example:"718926735" doc:"unique identifier for this specific node"`
+	}
 }
 
 // Response for /hello
@@ -49,7 +51,6 @@ type HelloResp struct {
 	Body struct {
 		Id uint64 `json:"id" required:"true" example:"123" doc:"unique identifier for the VK"`
 		//Message string `json:"message" example:"Hello, world!" doc:"response to a greeting"`
-		Error  string `json:"error,omitempty" example:"bad identifier (0)" doc:"the hello request was malformed or invalid"`
 		Height uint16 `json:"height" required:"true" example:"8" doc:"the height of the node answering the greeting"`
 	}
 }
@@ -57,7 +58,7 @@ type HelloResp struct {
 // Handle requests against the HELLO endpoint
 func handleHELLO(ctx context.Context, req *HelloReq) (*HelloResp, error) {
 	// validate their ID
-	if req.Id == 0 {
+	if req.Body.Id == 0 {
 		return nil, huma.Error400BadRequest(ErrBadID)
 	}
 
