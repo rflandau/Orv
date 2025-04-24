451048403d8287662f40fb3f841140a500fa9480
rflandau <rflandau@pm.me>
2025-04-23 22:13:45 -0700

    init orv_test

---
+++ pkg/orv/orv_test.go
@@ -0,0 +1,9 @@
+package orv_test
+
+import "testing"
+
+// Simple but important test to guarantee proper acceptance and rejection of message types to each endpoint.
+// A la ClientArgs in lab3.
+func TestEndpointArgs(t *testing.T) {
+
+}
