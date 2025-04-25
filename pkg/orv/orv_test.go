a24f6db67f964b92899714332291cba5840a0f57
rflandau <rflandau@pm.me>
2025-04-24 20:05:10 -0700

    skeletonize first test

--- pkg/orv/orv_test.go
+++ pkg/orv/orv_test.go
@@ -1,9 +1,20 @@
 package orv_test
 
-import "testing"
+import (
+	"testing"
+)
 
 // Simple but important test to guarantee proper acceptance and rejection of message types to each endpoint.
 // A la ClientArgs in lab3.
 func TestEndpointArgs(t *testing.T) {
+	//addr, err := netip.ParseAddrPort("127.0.0.1:8080")
+	// error check
 
+	// spin up vk
+	//vk, err := orv.NewVaultKeeper()
+	// error check
+
+	// send a bunch of garbage and out of order requests (ex joins before hello)
+
+	//http.Post()
 }
