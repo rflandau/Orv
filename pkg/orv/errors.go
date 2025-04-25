91bcf9495c4b204d63046ef0e2aabcfa9dccde1f
rflandau <rflandau@pm.me>
2025-04-24 22:15:48 -0700

    add root TODO

--- pkg/orv/errors.go
+++ pkg/orv/errors.go
@@ -25,5 +25,6 @@ type ErrBadHeight struct {
 
 func (e ErrBadHeight) Error() string {
 	// TODO if a parent is available, tell the requester to try the parent
+	// TODO if root and VKHeight==ReqHeight, send special MERGE error message
 	return fmt.Sprintf("To join this VK, height must be =%d-1 (given %d)", e.CurVKHeight, e.RequesterHeight)
 }
