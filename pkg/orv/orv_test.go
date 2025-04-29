04681ab219d5438d3415a176eb1820435e4fcf54
rflandau <rflandau@pm.me>
2025-04-28 21:58:36 -0700

    WIP convert humatest to resty

--- pkg/orv/orv_test.go
+++ pkg/orv/orv_test.go
@@ -5,19 +5,40 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"maps"
 	"net/http"
 	"net/http/httptest"
 	"net/netip"
 	"network-bois-orv/pkg/orv"
+	"slices"
 	"strconv"
 	"testing"
 	"time"
 
 	"github.com/danielgtaylor/huma/v2"
 	"github.com/danielgtaylor/huma/v2/humatest"
+	"resty.dev/v3"
 )
 
 //#region request helper functions and structs
+
+// Humatest is quite verbose by default and has no native way to suppress its logging.
+// To keep the tests readable (while still gaining the functionality of humatest), we wrap the test handler and give it no-op logging.
+// This way, humatest's native output is suppressed, but we can still utilize all the normal features of the test handler
+// (using the unwrapped version).
+type SuppressedLogTest struct {
+	*testing.T
+}
+
+// no-op wrapper
+func (tb SuppressedLogTest) Log(args ...any) {
+	// no-op
+}
+
+// no-op wrapper
+func (tb SuppressedLogTest) Logf(format string, args ...any) {
+	// no-op
+}
 
 const (
 	HelloSuccessCode    int = 200
@@ -35,19 +56,24 @@ type leaf struct {
 
 // POSTs a HELLO to the endpoint embedded in the huma api.
 // Only returns if the given status code was matched; Fatal if not
-func makeHelloRequest(t *testing.T, targetAPI humatest.TestAPI, expectedCode int, id uint64) (resp *httptest.ResponseRecorder) {
-	resp = targetAPI.Post(orv.EP_HELLO,
-		"Packet-Type: "+orv.PT_HELLO,
-		orv.HelloReq{
-			Body: struct {
-				Id uint64 "json:\"id\" required:\"true\" example:\"718926735\" doc:\"unique identifier for this specific node\""
-			}{
-				Id: id,
-			}}.Body)
-	if resp.Code != expectedCode {
-		t.Fatal("hello request failed: " + ErrBadResponseCode(resp.Code, expectedCode))
+func makeHelloRequest(t *testing.T, targetAddr netip.AddrPort, expectedCode int, id uint64) (*resty.Response, orv.HelloResp) {
+	cli := resty.New()
+	unpackedResp := orv.HelloResp{}
+	resp, err := cli.R().
+		SetBody(orv.HelloReq{Body: struct {
+			Id uint64 "json:\"id\" required:\"true\" example:\"718926735\" doc:\"unique identifier for this specific node\""
+		}{id}}.Body). // default request content type is JSON
+		SetExpectResponseContentType(orv.CONTENT_TYPE).
+		SetResult(&(unpackedResp.Body)).
+		Post("http://" + targetAddr.String() + orv.EP_HELLO)
+	if err != nil {
+		t.Fatal(err)
 	}
-	return resp
+
+	if resp.StatusCode() != expectedCode {
+		t.Fatal("hello request failed: " + ErrBadResponseCode(resp.StatusCode(), expectedCode))
+	}
+	return resp, unpackedResp
 }
 
 // POSTs a JOIN to the endpoint embedded in the huma api.
@@ -134,6 +160,15 @@ func sendServiceHeartbeats(targetAPI humatest.TestAPI, frequency time.Duration, 
 	return
 }
 
+// Checks if an error is currently waiting on the given error response channel and fails the test if it is.
+func checkHeartbeatError(t *testing.T, errResp chan *httptest.ResponseRecorder) {
+	select {
+	case e := <-errResp:
+		t.Fatalf("response code %d with response %s", e.Code, e.Body.String())
+	default:
+	}
+}
+
 //#endregion
 
 //#region testing error messages
@@ -201,13 +236,13 @@ func TestEndpointArgs(t *testing.T) {
 	t.Cleanup(vk.Terminate)
 
 	// submit a valid HELLO
-	makeHelloRequest(t, api, 200, 2)
+	makeHelloRequest(t, vkAddr, 200, 2)
 
 	// submit a valid JOIN
 	makeJoinRequest(t, api, 202, 2, 0, "", false)
 
 	// submit a HELLO with an invalid ID
-	makeHelloRequest(t, api, 400, 0)
+	makeHelloRequest(t, vkAddr, 400, 0)
 
 	// submit a JOIN with an invalid ID and height
 	makeJoinRequest(t, api, 400, 0, 0, "", false)
@@ -219,13 +254,13 @@ func TestEndpointArgs(t *testing.T) {
 	makeRegisterRequest(t, api, 400, 3, "Good advice generator - Just drink Milk instead of Coffee", vkAddr.String(), time.Second.String())
 
 	// submit a REGISTER for an unjoined ID
-	makeHelloRequest(t, api, 200, 3)
+	makeHelloRequest(t, vkAddr, 200, 3)
 	makeRegisterRequest(t, api, 400, 3, "Very good Coffee Maker - I might just beat Starbucks", vkAddr.String(), time.Second.String())
 
 	fmt.Println("ok")
 
 	// submit a valid REGISTER for vk
-	makeHelloRequest(t, api, 4, 100)
+	makeHelloRequest(t, vkAddr, 4, 100)
 	makeJoinRequest(t, api, 202, 4, 1, "", true)
 	makeRegisterRequest(t, api, 4, 100, "Horrible Coffee Maker", vkAddr.String(), time.Second.String())
 
@@ -238,45 +273,123 @@ func TestEndpointArgs(t *testing.T) {
 	fmt.Println("ok")
 
 	// submit a valid HELLO to the same ID as VK
-	makeHelloRequest(t, api, 400, 1)
+	makeHelloRequest(t, vkAddr, 400, 1)
 	makeJoinRequest(t, api, 400, 1, 0, "", false)
 	makeRegisterRequest(t, api, 400, 1, "Flopped Coffee", vkAddr.String(), time.Second.String())
 
 }
 
-// Tests that a single VK can support multiple leaves and multiple services on each leaf simultaneously.
+// Tests that a single VK can support multiple leaves and multiple services (some duplicates) on each leaf simultaneously.
 // Each leaf will HELLO -> JOIN and then submit multiple REGISTERS. Each service will need to send heartbeats to the VK.
-// After a short detail, the test checks if the VK still believe that all services are active.
+// After a short delay, the test checks if the VK still believe that all services are active.
 func TestMultiLeafMultiService(t *testing.T) {
-	// spawn the huma test API
-	_, api := humatest.New(t)
+	// wrap the test in a no-op for huma
+	nt := SuppressedLogTest{t}
 
-	vk, vkAddr := StartVKListener(t, api, 1)
+	// spawn the huma test api
+	_, api := humatest.New(nt)
 
-	time.Sleep(1 * time.Second) // give the VK time to start up
+	addr, err := netip.ParseAddrPort("[::1]:42069")
+	if err != nil {
+		t.Fatal(err)
+	}
 
+	vk, err := orv.NewVaultKeeper(1, addr, orv.SetHumaAPI(api))
+	if err != nil {
+		t.Fatal("failed to construct parent VK: ", err)
+	}
+	if err := vk.Start(); err != nil {
+		t.Fatal("failed to start parent VK: ", err)
+	}
 	t.Cleanup(vk.Terminate)
 
-	// submit a valid REGISTER for vk
-	makeHelloRequest(t, api, 200, 5)
-	makeJoinRequest(t, api, 200, 5, 0, "", false)
-	makeRegisterRequest(t, api, 200, 1, "Cookie Maker - as coffee maker bombed", vkAddr.String(), time.Second.String())
+	// populate the leaf information
+	lA, lB, lC := leaf{
+		id: 100, services: map[string]struct {
+			addr  string
+			stale string
+		}{
+			"ssh":  {addr: "127.0.0.1:6001", stale: "1s"},
+			"http": {addr: "127.0.0.1:6002", stale: "1s"}}},
+		leaf{id: 200, services: map[string]struct {
+			addr  string
+			stale string
+		}{
+			"ssh": {addr: "127.0.0.1:6011", stale: "1s"},
+			"dns": {addr: "127.0.0.1:6012", stale: "1s200ms"}}},
+		leaf{id: 300, services: map[string]struct {
+			addr  string
+			stale string
+		}{
+			"some longish service name": {addr: "127.0.0.1:6021", stale: "1s"},
+			"who even knows, man":       {addr: "127.0.0.1:6022", stale: "800ms"}}}
+	// register each leaf under the VK and start heartbeats for it
+	makeHelloRequest(t, addr, orv.EXPECTED_STATUS_HELLO, lA.id)
+	makeJoinRequest(t, api, orv.EXPECTED_STATUS_JOIN, lA.id, 0, "", false)
+	makeRegisterRequest(t, api, orv.EXPECTED_STATUS_REGISTER, lA.id, "ssh", lA.services["ssh"].addr, lA.services["ssh"].stale)
+	makeRegisterRequest(t, api, orv.EXPECTED_STATUS_REGISTER, lA.id, "http", lA.services["http"].addr, lA.services["http"].stale)
+	makeHelloRequest(t, addr, orv.EXPECTED_STATUS_HELLO, lB.id)
+	makeJoinRequest(t, api, orv.EXPECTED_STATUS_JOIN, lB.id, 0, "", false)
+	makeRegisterRequest(t, api, orv.EXPECTED_STATUS_REGISTER, lB.id, "ssh", lB.services["ssh"].addr, lB.services["ssh"].stale)
+	makeRegisterRequest(t, api, orv.EXPECTED_STATUS_REGISTER, lB.id, "dns", lB.services["dns"].addr, lB.services["dns"].stale)
+	lADone, lAErr := sendServiceHeartbeats(api, 300*time.Millisecond, lA.id, slices.Collect(maps.Keys(lA.services)))
+	lBDone, lBErr := sendServiceHeartbeats(api, 300*time.Millisecond, lB.id, slices.Collect(maps.Keys(lB.services)))
+	makeHelloRequest(t, addr, orv.EXPECTED_STATUS_HELLO, lC.id)
+	makeJoinRequest(t, api, orv.EXPECTED_STATUS_JOIN, lC.id, 0, "", false)
+	makeRegisterRequest(t, api, orv.EXPECTED_STATUS_REGISTER, lC.id, "some longish service name", lC.services["some longish service name"].addr, lC.services["some longish service name"].stale)
+	makeRegisterRequest(t, api, orv.EXPECTED_STATUS_REGISTER, lC.id, "who even knows, man", lC.services["who even knows, man"].addr, lC.services["who even knows, man"].stale)
+	lCDone, lCErr := sendServiceHeartbeats(api, 300*time.Millisecond, lC.id, slices.Collect(maps.Keys(lC.services)))
+	t.Cleanup(func() { close(lADone) })
+	t.Cleanup(func() { close(lBDone) })
+	t.Cleanup(func() { close(lCDone) })
 
-	makeHelloRequest(t, api, 200, 6)
-	makeJoinRequest(t, api, 200, 6, 0, "", false)
-	makeRegisterRequest(t, api, 200, 1, "CMU Cycle stand Manager - too many unaccounted cycles", vkAddr.String(), time.Second.String())
+	// allow leaves to expire if they are going
+	time.Sleep(2 * time.Second)
+	select {
+	case e := <-lAErr:
+		t.Fatalf("response code %d with response %s", e.Code, e.Body.String())
+	default:
+	}
+	select {
+	case e := <-lBErr:
+		t.Fatalf("response code %d with response %s", e.Code, e.Body.String())
+	default:
+	}
+	select {
+	case e := <-lCErr:
+		t.Fatalf("response code %d with response %s", e.Code, e.Body.String())
+	default:
+	}
 
-	makeHelloRequest(t, api, 200, 7)
-	makeJoinRequest(t, api, 200, 7, 0, "", false)
-	makeRegisterRequest(t, api, 200, 1, "Broken Laptop service - Honestly, nothing you can do about it", vkAddr.String(), time.Second.String())
+	// check that the VK believes it has the correct number of children, services, and service providers
+	snap := vk.ChildrenSnapshot()
+	if len(snap.CVKs) > 0 {
+		t.Fatal("expected to find zero cVKs, found ", len(snap.CVKs))
+	}
+	if len(snap.Leaves) != 3 {
+		t.Fatal("expected to find 3 leaves, found ", len(snap.Leaves))
+	}
+	if sshProviders := snap.Services["ssh"]; len(sshProviders) != 2 {
+		t.Fatal("expected to find 2 providers of the ssh service, found ", len(sshProviders))
+	}
+	if providerCount := len(snap.Services["http"]); providerCount != 1 {
+		t.Fatal("expected to find 1 providers of the http service, found ", providerCount)
+	}
+	if providerCount := len(snap.Services["dns"]); providerCount != 1 {
+		t.Fatal("expected to find 1 providers of the dns service, found ", providerCount)
+	}
 
 }
 
-// Tests that we can build and query a small vault.
+// Tests that a VK will automatically prune out individual services that do not heartbeat and all services learned by a child VK when the cVK does not heartbeat.
+func TestAutoPrune(t *testing.T) {
+	// TODO
+	t.Fatal("NYI")
+}
+
+// Tests that we can build a small vault without merging
 // Composes a tree of the form LeafA --> VKA --> VKB <-- LeafB, including consistent heartbeats for leaves (and the self-managing heartbeats inherent to VKs).
 // VKB and VKA do not merge; instead VKB is spawned with a Dragon's Hoard of 1 and VKA joins as a child.
-//
-// Tests each part of the tree individually (each VK knows about its children and the services they offer) and tests a bubble-up list request.
 func TestSmallVault(t *testing.T) {
 	vkAAddr, err := netip.ParseAddrPort("[::1]:8090")
 	if err != nil {
@@ -310,27 +423,35 @@ func TestSmallVault(t *testing.T) {
 	if err := vkA.Join(vkB.AddrPort().String()); err != nil {
 		t.Fatal(err)
 	}
-	//t.Log(makeJoinRequest(t, apiB, 202, vkA.ID(), vkA.Height(), vkAAddr.String(), true))
 
-	/*
-		// start to send heartbeats A --> B
-		aHBDone, aHBErr := sendVKHeartbeats(apiB, 1*time.Second, vkA.ID())
-		t.Cleanup(func() { close(aHBDone) })
+	// join leaf B under B
+	lB := leaf{id: 200, services: map[string]struct {
+		addr  string
+		stale string
+	}{"temp": {"2.2.2.2:99", "2s"}}}
+	makeHelloRequest(t, vkBAddr, orv.EXPECTED_STATUS_HELLO, lB.id)
+	makeJoinRequest(t, apiB, orv.EXPECTED_STATUS_JOIN, lB.id, 0, "", false)
+	makeRegisterRequest(t, apiB, orv.EXPECTED_STATUS_REGISTER, lB.id, "temp", lB.services["temp"].addr, lB.services["temp"].stale)
+	lBDone, lBErr := sendServiceHeartbeats(apiB, 500*time.Millisecond, lB.id, []string{"temp"})
+	t.Cleanup(func() { close(lBDone) })
+	// join leaf A under A
+	lA := leaf{id: 100, services: map[string]struct {
+		addr  string
+		stale string
+	}{"temp": {"1.1.1.1:99", "1s700ms"}}}
+	makeHelloRequest(t, vkAAddr, orv.EXPECTED_STATUS_HELLO, lA.id)
+	makeJoinRequest(t, apiA, orv.EXPECTED_STATUS_JOIN, lA.id, 0, "", false)
+	makeRegisterRequest(t, apiA, orv.EXPECTED_STATUS_REGISTER, lA.id, "temp", lA.services["temp"].addr, lA.services["temp"].stale)
+	lADone, lAErr := sendServiceHeartbeats(apiA, 500*time.Millisecond, lA.id, []string{"temp"})
+	t.Cleanup(func() { close(lADone) })
 
-		// sleep long enough for non-heartbeat VKs to get pruned
-		time.Sleep(orv.DEFAULT_PRUNE_TIME_CVK + 1*time.Second)
+	time.Sleep(6 * time.Second)
 
-		// check for any heartbeating errors
-		select {
-		case e := <-aHBErr:
-			// an error occurred in the heartbeater
-			t.Fatalf("response code %d with response %s", e.Code, e.Body.String())
-		default:
-			// the heartbeater worked as intended
-		}
+	// check for heartbeater errors
+	checkHeartbeatError(t, lBErr)
+	checkHeartbeatError(t, lAErr)
 
-	*/
-	// check that A is still included as a child of B
+	// check that B still considers A and LeafB to be its children and that it is aware that leafB offers 1 service.
 	snap := vkB.ChildrenSnapshot()
 	if _, exists := snap.CVKs[vkA.ID()]; !exists {
 		t.Fatal("cVK A was pruned from B despite no heartbeater errors. Snapshot:", snap)
@@ -341,84 +462,8 @@ func TestSmallVault(t *testing.T) {
 		t.Fatal("A does not believe B is its parent")
 	}
 
-	// register a leaf to A
-	/*
-		leafA := leaf{
-			id: 100,
-			services: map[string]struct {
-				addr  string
-				stale string
-			}{"ssh": {"127.0.0.1:22", "1s"}},
-		}
+	// TODO
 
-					makeHelloRequest(t, apiA, HelloSuccessCode, leafA.id)
-					makeJoinRequest(t, apiA, JoinSuccessCode, leafA.id, 0, "", false)
-					makeRegisterRequest(t, apiA, RegisterSuccessCode, leafA.id, "ssh", leafA.services["ssh"].addr, leafA.services["ssh"].stale)
-
-				leafAServiceHBDone, leafAServiceHBErr := sendServiceHeartbeats(apiA, 500*time.Millisecond, leafA.id, []string{"ssh"})
-				t.Cleanup(func() { close(leafAServiceHBDone) })
-			// sleep long enough for greater than service stale time
-			time.Sleep(1500 * time.Millisecond)
-
-			// check for any heartbeating errors
-			select {
-			case e := <-leafAServiceHBErr:
-				// an error occurred in the heartbeater
-				t.Fatalf("response code %d with response %s", e.Code, e.Body.String())
-			default:
-				// the heartbeater worked as intended
-			}
-
-			// check that A has a leaf with leafA's ID
-			snap = vkA.ChildrenSnapshot()
-			if _, exists := snap.Leaves[leafA.id]; !exists {
-				t.Fatal("LeafA was pruned from A despite no heartbeater errors. Snapshot:", snap)
-			}
-			// check that A has a service "ssh"
-			if _, exists := snap.Services["ssh"]; !exists {
-				t.Fatal("service 'ssh' was not found on vk A despite no heartbeater errors. Snapshot:", snap)
-			}
-
-			// register a leaf to B
-			leafB := leaf{
-				id: 200,
-				services: map[string]struct {
-					addr  string
-					stale string
-				}{"DNS": {"111.0.0.1:53", "1s"}},
-			}
-			makeHelloRequest(t, apiB, HelloSuccessCode, leafB.id)
-			makeJoinRequest(t, apiB, JoinSuccessCode, leafB.id, 0, "", false)
-			makeRegisterRequest(t, apiB, RegisterSuccessCode, leafB.id, "DNS", leafB.services["DNS"].addr, leafB.services["DNS"].stale)
-
-			leafBServiceHBDone, leafBServiceHBErr := sendServiceHeartbeats(apiB, 500*time.Millisecond, leafB.id, []string{"DNS"})
-			t.Cleanup(func() { close(leafBServiceHBDone) })
-
-			// sleep long enough for greater than service stale time
-			time.Sleep(1500 * time.Millisecond)
-
-			// check for any heartbeating errors
-			select {
-			case e := <-leafBServiceHBErr:
-				// an error occurred in the heartbeater
-				t.Fatalf("response code %d with response %s", e.Code, e.Body.String())
-			default:
-				// the heartbeater worked as intended
-			}
-
-			// check that B has a leaf with leafB's ID
-			snap = vkB.ChildrenSnapshot()
-			if _, exists := snap.Leaves[leafB.id]; !exists {
-				t.Fatal("LeafB was pruned from B despite no heartbeater errors. Snapshot:", snap)
-			}
-			// check that B has a service "DNS"
-			if _, exists := snap.Services["DNS"]; !exists {
-				t.Fatal("service 'DNS' was not found on vk B despite no heartbeater errors. Snapshot:", snap)
-			}
-
-			// we should now be able to submit a list request to A which will propagate up to ... TODO
-	*/
-	time.Sleep(time.Second)
 }
 
 // Tests that VKs properly prune out leaves that do not register at least one service within a short span AND that
@@ -468,7 +513,7 @@ func TestLeafNoRegisterNoHeartbeat(t *testing.T) {
 		},
 	}
 	// introduce and join leaf A
-	makeHelloRequest(t, api, 200, leafA.id)
+	makeHelloRequest(t, vkAddr, 200, leafA.id)
 	makeJoinRequest(t, api, 202, leafA.id, 0, "", false)
 	// register the service
 	makeRegisterRequest(t, api, 202, leafA.id, "testServiceA", leafA.services["testServiceA"].addr, leafA.services["testServiceA"].stale)
