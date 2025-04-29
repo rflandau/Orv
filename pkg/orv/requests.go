60e68b110fb1f42cae80f82be5aef14e2bd42541
rflandau <rflandau@pm.me>
2025-04-28 19:18:44 -0700

    create status caller subroutine

---
+++ pkg/orv/requests.go
@@ -0,0 +1,32 @@
+package orv
+
+import (
+	"strings"
+
+	"resty.dev/v3"
+)
+
+/*
+This file contains requests that can be called on VKs from any client.
+*/
+
+// Spawns a new resty client and uses it to make a status request against the target address.
+// Serves as an example of how to make client-side calls against a Vault.
+//
+// addrStr should be of the form "http://<ip>:<port>"
+func Status(addrStr string) (*resty.Response, StatusResp, error) {
+	cli := resty.New()
+
+	// compose the url
+	addrStr = strings.TrimSuffix(addrStr, "/")
+	url := addrStr + EP_STATUS
+
+	sr := StatusResp{}
+
+	res, err := cli.R().
+		SetExpectResponseContentType(CONTENT_TYPE).
+		SetResult(&(sr.Body)).
+		Get(url)
+	return res, sr, err
+
+}
