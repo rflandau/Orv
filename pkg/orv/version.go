6ebcf49c0ed4f5460d87706484c92122a7901cee
rflandau <rflandau@pm.me>
2025-04-29 22:33:54 -0700

    increment version

--- pkg/orv/version.go
+++ pkg/orv/version.go
@@ -2,5 +2,5 @@ package orv
 
 const (
 	_API_NAME    string = "Orv"   // the API is for ORV
-	_API_VERSION string = "1.0.0" // the Orv version being served
+	_API_VERSION string = "1.0.1" // the Orv version being served
 )
