163b8f3edcd920205e95defc073d2f7992f930e2
R Landau <rflandau@pm.me>
2025-08-13 09:52:29 -0700

    add version field

---
+++ pkg/orvCoAP/proto/version.go
@@ -0,0 +1,35 @@
+package proto
+
+import "errors"
+
+type Version struct {
+	// Orv version minor number.
+	// Only 4 least-significant bits are used.
+	Major uint8
+	// Orv version minor number.
+	// Only 4 least-significant bits are used.
+	Minor uint8
+}
+
+// Byte returns Orv version as a single byte.
+// The most-significant nibble is major; the rest are minor.
+// Ex: 0b 0010 0001 equates to version 2.1
+func (v Version) Byte() uint8 {
+	return v.Major<<4 | v.Minor&0b00001111
+}
+
+// NewVersion returns a version struct after ensuring that major and minor can each fit into a nibble.
+func NewVersion(major, minor uint8) (Version, error) {
+	if major > 15 {
+		return Version{}, errors.New("major must be able to fit into a nibble (4 bits, <= 0xF)")
+	} else if minor > 15 {
+		return Version{}, errors.New("minor must be able to fit into a nibble (4 bits, <= 0xF)")
+	}
+
+	return Version{major, minor}, nil
+}
+
+// VersionFromByte splits the byte into two nibbles, setting them to major and minor respectively.
+func VersionFromByte(b byte) Version {
+	return Version{b >> 4, b & 0b00001111}
+}
---
+++ pkg/orvCoAP/proto/version_test.go
@@ -0,0 +1,65 @@
+// Package proto contains tools for interacting with the L5 orv header.
+// Includes structs that can be composed into a fixed header; you should never have to interact with the raw bits or endian-ness of the header.
+package proto_test
+
+import (
+	"network-bois-orv/pkg/orvCoAP/proto"
+	"testing"
+)
+
+// Tests creating a new version from uint8 major and minors and that the byte .Byte() returns is as expected.
+func TestVersion(t *testing.T) {
+	type args struct {
+		major uint8
+		minor uint8
+	}
+	type want struct {
+		B   byte
+		err bool
+	}
+	tests := []struct {
+		name string
+		args args
+		want want
+	}{
+		{"invalid major", args{16, 1}, want{0b0, true}},
+		{"invalid minor", args{2, 255}, want{0b0, true}},
+		{"2.1", args{2, 1}, want{0b00100001, false}},
+		{"15.15", args{15, 15}, want{0b11111111, false}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			v, err := proto.NewVersion(tt.args.major, tt.args.minor)
+			if (err != nil) != tt.want.err {
+				t.Errorf("NewVersion() error = %v, wantErr %v", err, tt.want.err)
+				return
+			}
+			if v.Byte() != tt.want.B {
+				t.Errorf("NewVersion() = %v, want %v", v, tt.want)
+			}
+		})
+	}
+}
+
+func TestVersionFromByte(t *testing.T) {
+	tests := []struct {
+		name string
+		b    byte
+		want proto.Version
+	}{
+		{"0.0", 0b00000000, proto.Version{0, 0}},
+		{"0.0", 0b0, proto.Version{0, 0}},
+		{"0.1", 0b1, proto.Version{0, 1}},
+		{"1.0", 0b00010000, proto.Version{1, 0}},
+		{"1.1", 0b00010001, proto.Version{1, 1}},
+		{"15.15", 0b11111111, proto.Version{15, 15}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := proto.VersionFromByte(tt.b)
+			if got.Major != tt.want.Major || got.Minor != tt.want.Minor {
+				t.Fatalf("expected %v, actual %v", tt.want, got)
+			}
+		})
+	}
+}
