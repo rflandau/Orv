11a9a037b4c8b658f559651b28ba1986945eaf4c
R Landau <rflandau@pm.me>
2025-08-13 10:31:45 -0700

    WIP define packet header; include length and type

--- pkg/orvCoAP/proto/proto.go
+++ pkg/orvCoAP/proto/proto.go
@@ -1,5 +1,17 @@
-// Package proto contains tools for interacting with the L5 orv header.
-// Includes structs that can be composed into a fixed header; you should never have to interact with the raw bits or endian-ness of the header.
+/*
+Package proto contains tools for interacting with the L5 orv header.
+
+Includes structs that can be composed into a fixed header; you should never have to interact with the raw bits or endian-ness of the header.
+
+	    0               1               2               3
+	    0 1 2 3 4 5 6 7 0 1 2 3 4 5 6 7 0 1 2 3 4 5 6 7 0 1 2 3 4 5 6 7
+
+		+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+		| Major | Minor |   Hop Limit   |    Total Length in Octets   |
+		+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+		|     Type      |
+		+-+-+-+-+-+-+-+-+
+*/
 package proto
 
 import (
@@ -7,28 +19,39 @@ import (
 	"encoding/binary"
 	"errors"
 	"fmt"
+	"strconv"
 )
 
 // FixedHeaderLen is the length (in bytes) of the Orv fixed header.
-const FixedHeaderLen uint = 2
+const FixedHeaderLen uint = 5
 
+// A Header represents a deconstructed Orv packet header.
+// Use SerializeTo()/SerializeFrom().
 type Header struct {
 	Version  Version
 	HopLimit uint8
+	// length of the payload.
+	// Must be <= (65535-FixedHeaderLen)
+	PayloadLength uint16
+	Type          MessageType
 }
 
-// ErrInvalidHopLimit means that the given hop limit was out of bounds.
-var ErrInvalidHopLimit = errors.New("HopLimit must be 1 <= x <= 255 ")
+var (
+	// ErrInvalidHopLimit means that the given hop limit was out of bounds.
+	ErrInvalidHopLimit      = errors.New("HopLimit must be 1 <= x <= 255")
+	ErrInvalidPayloadLength = errors.New("PayloadLength must be 0 <= x <= " + strconv.FormatUint(uint64(65535-FixedHeaderLen), 10) + " (uint16MAX - length of the fixed header)")
+)
 
 // SerializeTo consumes the data required to compose an Orv header and returns a network-order byte string.
-// Also validates that header has all required fields.
+// Also validates that the header has all required fields.
 func (hdr *Header) SerializeTo() ([]byte, error) {
 	if hdr.HopLimit < 1 {
 		return nil, ErrInvalidHopLimit
 	}
 
 	buf := make([]byte, FixedHeaderLen)
-	if count, err := binary.Encode(buf, binary.BigEndian, []byte{hdr.Version.Byte(), hdr.HopLimit}); err != nil {
+	if count, err := binary.Encode(buf, binary.BigEndian,
+		[]byte{hdr.Version.Byte(), hdr.HopLimit, byte(hdr.PayloadLength >> 8), byte(hdr.PayloadLength), hdr.Type}); err != nil {
 		return nil, err
 	} else if count != int(FixedHeaderLen) {
 		var moreOrFewer = "fewer"
@@ -60,22 +83,35 @@ func (hdr *Header) SerializeFrom(rd *bytes.Reader) (err error) {
 	return nil
 }
 
-//#region FieldOrder
+//#region MessageType
 
-type FieldOrder = uint
-
-const (
-	VersionMajorOrder FieldOrder = iota
-	VersionMinorOrder
-	HopLimitFieldOrder
-)
-
-//#endregion FieldOrder
-
-type FieldToByte uint
+// MessageType enumerates the message types and the integers used to represent them in the Type field.
+type MessageType = uint8
 
 const (
-	VersionMajorByte FieldToByte = 0
-	VersionMinorByte FieldToByte = 0
-	HopLimitByte     FieldToByte = 1
+	Hello MessageType = iota
+	HelloAck
+
+	Join
+	JoinAccept
+	JoinDeny
+
+	Register
+	RegisterAccept
+	RegisterDeny
+
+	Merge
+	MergeAccept
+	Increment
+	IncrementACK
+
+	ServiceHeartbeat
+	ServiceHeartbeatAck
+	ServiceHeartbeatFault
+
+	VKHeartbeat
+	VKHeartbeatAck
+	VKHeartbeatFault
 )
+
+//#endregion MessageType
