74b23350a0e17bb76f9ebceab0d0eafdd6916a5e
rflandau <rflandau@pm.me>
2025-08-20 11:46:37 -0700

    add shared Orv package

---
+++ implementations/slims/orv/orv.go
@@ -0,0 +1,17 @@
+// Package orv is the parent package of the Orv implementation.
+// It contains child packages vk (vault keeper implementation), proto (L5 Orv header handling), and client (static subroutines for interacting with a vault).
+// Child packages are mostly self-contained, the Orv parent package provides the few shared utilities.
+package orv
+
+// VK or Leaf's unique identifier
+type NodeID = uint64
+
+// StatusResponse is the information sent in response to a STATUS packet.
+type StatusResponse struct {
+	// ID of the VK responding to the Status
+	ID     NodeID `json:"id,omitempty"`
+	Height uint16 `json:"height,omitempty"`
+	// packed bytes; MSN is major, LSN is minor
+	VersionsSupported []byte `json:"versions_supported,omitempty"`
+	// TODO
+}
