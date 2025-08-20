8fff6556f826ac998e8f63b536f7f678c0bd9448
rflandau <rflandau@pm.me>
2025-08-20 15:09:01 -0700

    extract ping to testsupport.CoAPPing()

--- implementations/slims/orv/vk/vaultkeeper_test.go
+++ implementations/slims/orv/vk/vaultkeeper_test.go
@@ -4,10 +4,9 @@ import (
 	"context"
 	"errors"
 	"net/netip"
+	. "network-bois-orv/internal/testsupport"
 	"testing"
 	"time"
-
-	"github.com/plgd-dev/go-coap/v3/udp"
 )
 
 func TestVaultKeeper_StartStop(t *testing.T) {
@@ -19,45 +18,30 @@ func TestVaultKeeper_StartStop(t *testing.T) {
 	const timeout time.Duration = 300 * time.Millisecond
 
 	vk.Start()
-	if err := ping("127.0.0.1:8081", timeout); err != nil {
+	if err := CoAPPing("127.0.0.1:8081", timeout); err != nil {
 		t.Error(err)
 	}
 	vk.Stop()
-	// ping the server again, but expect failure
-	if err := ping("127.0.0.1:8081", timeout); err == nil || !errors.Is(err, context.DeadlineExceeded) {
-		t.Error("bad ping result. Expected DeadlineExceeded error, found ", err)
+	// testsupport.CoAPPing the server again, but expect failure
+	if err := CoAPPing("127.0.0.1:8081", timeout); err == nil || !errors.Is(err, context.DeadlineExceeded) {
+		t.Error("bad testsupport.CoAPPing result. Expected DeadlineExceeded error, found ", err)
 	}
 
 	vk.Start()
-	if err := ping("127.0.0.1:8081", timeout); err != nil {
+	if err := CoAPPing("127.0.0.1:8081", timeout); err != nil {
 		t.Error(err)
 	}
 	vk.Stop()
-	if err := ping("127.0.0.1:8081", timeout); err == nil || !errors.Is(err, context.DeadlineExceeded) {
-		t.Error("bad ping result. Expected DeadlineExceeded error, found ", err)
+	if err := CoAPPing("127.0.0.1:8081", timeout); err == nil || !errors.Is(err, context.DeadlineExceeded) {
+		t.Error("bad testsupport.CoAPPing result. Expected DeadlineExceeded error, found ", err)
 	}
 
 	vk.Start()
-	if err := ping("127.0.0.1:8081", timeout); err != nil {
+	if err := CoAPPing("127.0.0.1:8081", timeout); err != nil {
 		t.Error(err)
 	}
 	vk.Stop()
-	if err := ping("127.0.0.1:8081", timeout); err == nil || !errors.Is(err, context.DeadlineExceeded) {
-		t.Error("bad ping result. Expected DeadlineExceeded error, found ", err)
+	if err := CoAPPing("127.0.0.1:8081", timeout); err == nil || !errors.Is(err, context.DeadlineExceeded) {
+		t.Error("bad testsupport.CoAPPing result. Expected DeadlineExceeded error, found ", err)
 	}
-}
-
-func ping(addr string, timeout time.Duration) error {
-	// ping the server
-	conn, err := udp.Dial(addr)
-	if err != nil {
-		return err
-	}
-	defer conn.Close()
-	ctx, cancel := context.WithTimeout(context.Background(), timeout)
-	defer cancel()
-	if err := conn.Ping(ctx); err != nil {
-		return err
-	}
-	return nil
 }
--- internal/testsupport/testsupport.go
+++ internal/testsupport/testsupport.go
@@ -1,10 +1,32 @@
 // Package testsupport is an internal-only package that provides utilities for testing uniformity.
 package testsupport
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/plgd-dev/go-coap/v3/udp"
+)
 
 // ExpectedActual returns a newline-prefixed string comparing the expected result to the actual result.
 // Should be used to add clarity to unit test error messages.
 func ExpectedActual(expected, actual any) string {
 	return fmt.Sprintf("\n\tExpected: '%v'\n\tActual: '%v'", expected, actual)
+}
+
+// CoApPing is a helper function that sends a ping to the given address.
+func CoAPPing(addr string, timeout time.Duration) error {
+	// ping the server
+	conn, err := udp.Dial(addr)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+	if err := conn.Ping(ctx); err != nil {
+		return err
+	}
+	return nil
 }
