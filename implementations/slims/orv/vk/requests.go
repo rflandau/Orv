6f196c39327c9fda619defae78f591c711ca1ec2
rflandau <rflandau@pm.me>
2025-08-24 21:55:22 -0700

    use protocol buffer struct

--- implementations/slims/orv/vk/requests.go
+++ implementations/slims/orv/vk/requests.go
@@ -3,6 +3,7 @@ package vaultkeeper
 import (
 	"bytes"
 	"context"
+	"errors"
 	"fmt"
 	"io"
 
@@ -19,22 +20,16 @@ import (
 // These subroutines are used to communicate with other nodes.
 // Request payloads are encoded via protobuf.
 
-type HelloAck struct {
-	ID      orv.NodeID
-	Height  uint16
-	Version byte // major+minor as a byte
-}
-
 // Hello sends a HELLO packet to the given address, returning the target node's response or an error.
-func (vk *VaultKeeper) Hello(addrPort string, ctx context.Context) (response HelloAck, err error) {
+func (vk *VaultKeeper) Hello(addrPort string, ctx context.Context) (response *pb.HelloAck, err error) {
 	if !vk.alive.Load() {
-		return HelloAck{}, ErrDead
+		return nil, ErrDead
 	}
 
 	// compose the body
 	body, err := proto.Marshal(&pb.Hello{Id: vk.id})
 	if err != nil {
-		return HelloAck{}, err
+		return nil, err
 	}
 
 	// only sanity check length in debug mode
@@ -53,12 +48,12 @@ func (vk *VaultKeeper) Hello(addrPort string, ctx context.Context) (response Hel
 	}
 	reqHdrB, err := reqHdr.Serialize()
 	if err != nil {
-		return HelloAck{}, err
+		return nil, err
 	}
 
 	conn, err := udp.Dial(addrPort)
 	if err != nil {
-		return HelloAck{}, err
+		return nil, err
 	}
 	defer conn.Close()
 
@@ -66,36 +61,40 @@ func (vk *VaultKeeper) Hello(addrPort string, ctx context.Context) (response Hel
 	respBody := resp.Body()
 	var hdrBytes = make([]byte, protocol.FixedHeaderLen)
 	if n, err := respBody.Read(hdrBytes); err != nil {
-		return HelloAck{}, err
+		return nil, err
 	} else if n != int(protocol.FixedHeaderLen) {
-		return HelloAck{}, fmt.Errorf("incorrect read count (%d, expected %d)", n, protocol.FixedHeaderLen)
+		return nil, fmt.Errorf("incorrect read count (%d, expected %d)", n, protocol.FixedHeaderLen)
 	}
 
 	// read the header
 	respHeader := protocol.Header{}
 	if err := respHeader.Deserialize(bytes.NewBuffer(hdrBytes)); err != nil {
-		return HelloAck{}, err
+		return nil, err
 	}
 	// read the payload
 	// if the packet type is FAULT, unmarshal as a fault
 	if respHeader.Type == mt.Fault {
 
+	} else if respHeader.Type == mt.HelloAck {
+
+	} else {
+		return nil, errors.New("unexpected response message type: " + respHeader.Type.String())
 	}
 	// TODO the response body should already be read forward 5 bytes, but we need to confirm that
-	var ret HelloAck
+	var ret pb.HelloAck
 	if respBody, err := io.ReadAll(respBody); err != nil {
-		return HelloAck{}, err
+		return nil, err
 	} else {
 		var r pb.HelloAck
 		if err := proto.Unmarshal(respBody, &r); err != nil {
-			return HelloAck{}, err
+			return nil, err
 		}
 		// narrow the type to a HelloAck
-		ret = HelloAck{
-			ID:      r.Id,
-			Height:  uint16(r.Height),
-			Version: byte(r.Version),
+		ret = pb.HelloAck{
+			Id:      r.Id,
+			Height:  r.Height,
+			Version: r.Version,
 		}
 	}
-	return ret, nil
+	return &ret, nil
 }
