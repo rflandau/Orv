e5fa54184bcaeafca57e189db1eedf53287f3c97
R Landau <rflandau@pm.me>
2025-08-26 18:08:16 -0700

    define SlicesUnorderedEqual

--- internal/testsupport/testsupport.go
+++ internal/testsupport/testsupport.go
@@ -4,6 +4,8 @@ package testsupport
 import (
 	"context"
 	"fmt"
+	"maps"
+	"sync"
 	"time"
 
 	"github.com/plgd-dev/go-coap/v3/udp"
@@ -15,8 +17,36 @@ func ExpectedActual(expected, actual any) string {
 	return fmt.Sprintf("\n\tExpected: '%v'\n\tActual: '%v'", expected, actual)
 }
 
-// CoApPing is a helper function that sends a ping to the given address.
+// SlicesUnorderedEqual compares the elements of the given slices for equality and equal count without taking order of the elements into account.
+func SlicesUnorderedEqual[T comparable](a []T, b []T) bool {
+	// convert each slice into a map of key --> count
+	var wg sync.WaitGroup
+
+	am := make(map[T]uint)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for _, k := range a {
+			am[k] += 1
+		}
+	}()
+
+	bm := make(map[T]uint)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for _, k := range b {
+			bm[k] += 1
+		}
+	}()
+
+	wg.Wait()
+	return maps.Equal(am, bm)
+}
+
+// CoAPPing is a helper function that sends a ping to the given address.
 func CoAPPing(addr string, timeout time.Duration) error {
+	// TODO remove
 	// ping the server
 	conn, err := udp.Dial(addr)
 	if err != nil {
