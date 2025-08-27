e44f4447d3049e6f2a6dd5eb484e656f8ce3f87d
R Landau <rflandau@pm.me>
2025-08-26 18:08:30 -0700

    redefine the header

--- implementations/slims/orv/protocol/protocol.go
+++ implementations/slims/orv/protocol/protocol.go
@@ -1,14 +1,7 @@
 /*
-Package proto contains tools for interacting with the L5 orv header.
+Package protocol contains tools for interacting with the L5 orv header.
 
 Includes structs that can be composed into a fixed header; you should never have to interact with the raw bits or endian-ness of the header.
-
-	    0               1               2               3
-	    0 1 2 3 4 5 6 7 0 1 2 3 4 5 6 7 0 1 2 3 4 5 6 7 0 1 2 3 4 5 6 7
-
-		+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-		| Major | Minor |     Type      |    Total Length in Octets   |
-		+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 */
 package protocol
 
@@ -18,39 +11,41 @@ import (
 	"fmt"
 	"io"
 	"math"
-	"strconv"
 
+	"github.com/rflandau/Orv/implementations/slims/orv"
 	"github.com/rflandau/Orv/implementations/slims/orv/protocol/mt"
 	"github.com/rs/zerolog"
 )
 
 const (
 	// FixedHeaderLen is the length (in bytes) of the Orv fixed header.
-	FixedHeaderLen   uint   = 4
+	FixedHeaderLen uint = 12
+	// the maximum length the payload can be to fit into a single UDP datagram (without IPv6's jumbograms).
 	MaxPayloadLength uint16 = math.MaxUint16 - uint16(FixedHeaderLen)
 )
 
 // A Header represents a deconstructed Orv packet header.
 // The state of Header is never guaranteed; call .Validate() to verify before using..
 type Header struct {
-	// The highest major and minor version of Orv the sender speaks.
+	// Version of Orv Slims this message intends to use.
 	Version Version
+	// Does this packet omit the ID field?
+	Shorthand bool
 	// Type of message.
 	// See PACKETS.md for the available message types.
 	Type mt.MessageType
-	// Length of the payload in bytes.
-	// Must be <= (65535-FixedHeaderLen).
-	// Defaults to zero, thus telling the receiver not to scan any data after the header.
-	PayloadLength uint16
+	// Unique identifier of the sender. May be anything that fits into 8B, but is treated as an unsigned int in this implementation.
+	// Ignored if Shorthand is set.
+	ID orv.NodeID
 }
 
 //#region errors
 
 var (
-	ErrInvalidVersionMajor  = errors.New("major version must be 0 <= x <= 15")
-	ErrInvalidVersionMinor  = errors.New("minor version must be 0 <= x <= 15")
-	ErrInvalidPayloadLength = errors.New("payload length must be 0 <= x <= " + strconv.FormatUint(uint64(65535-FixedHeaderLen), 10) + " (uint16MAX - length of the fixed header)")
-	ErrUnknownMessageType   = errors.New("type must be a valid MessageType")
+	ErrInvalidVersionMajor = errors.New("major version must be 0 <= x <= 15")
+	ErrInvalidVersionMinor = errors.New("minor version must be 0 <= x <= 15")
+	ErrInvalidMessageType  = errors.New("message type must be representable with 7 bits and must not be 0")
+	ErrShorthandID         = errors.New("ID is ignored when shorthand is set")
 )
 
 //#endregion errors
@@ -63,7 +58,27 @@ var (
 // Performs a single allocation of FixedHeaderLen size.
 func (hdr *Header) Serialize() ([]byte, error) {
 	out := make([]byte, FixedHeaderLen)
-	data := []byte{hdr.Version.Byte(), byte(hdr.Type), byte(hdr.PayloadLength >> 8), byte(hdr.PayloadLength)}
+
+	var data []byte
+	if hdr.Shorthand {
+		data = []byte{
+			hdr.Version.Byte(),
+			0b10000000 | byte(hdr.Type),
+		}
+	} else {
+		data = []byte{
+			hdr.Version.Byte(),
+			0b00000000 | byte(hdr.Type),
+			byte(hdr.ID & 0xFF00000000000000),
+			byte(hdr.ID & 0x00FF000000000000),
+			byte(hdr.ID & 0x0000FF0000000000),
+			byte(hdr.ID & 0x000000FF00000000),
+			byte(hdr.ID & 0x00000000FF000000),
+			byte(hdr.ID & 0x0000000000FF0000),
+			byte(hdr.ID & 0x000000000000FF00),
+			byte(hdr.ID & 0x00000000000000FF),
+		}
+	}
 
 	// compose data into out
 	if count, err := binary.Encode(out, binary.BigEndian, data); err != nil {
@@ -78,13 +93,15 @@ func (hdr *Header) Serialize() ([]byte, error) {
 	return out, nil
 }
 
-// Deserialize greedily populates hdr's fields from the given byte array.
+// Deserialize populates hdr's fields from the given reader.
+// Reads 2 bytes if Shorthand, 12 otherwise.
 // Clobbers existing data.
 //
-// Returns when rd is empty or we have read FixedHeaderLen bytes, whichever is first.
 // Does NOT validate fields. Does not drain rd.
-// Swallows EOF errors and short reads.
+//
+// If an error occurs, hdr will not be altered and its state is considered undefined.
 func (hdr *Header) Deserialize(rd io.Reader) (err error) {
+	// set up a function to peel off a byte at a time
 	var buf = make([]byte, 1)
 	readByte := func() (b byte, done bool, err error) {
 		if read, err := rd.Read(buf); err != nil {
@@ -98,30 +115,37 @@ func (hdr *Header) Deserialize(rd io.Reader) (err error) {
 		return buf[0], false, nil // success
 	}
 
-	{ // Version
-		b, done, err := readByte()
-		if done || err != nil {
-			return err
-		}
+	// Version
+	if b, done, err := readByte(); err != nil {
+		return err
+	} else if done {
+		return errors.New("short read on byte 1 (Version)")
+	} else {
 		hdr.Version = VersionFromByte(b)
 	}
-	{ // Message Type
-		b, done, err := readByte()
-		if done || err != nil {
-			return err
-		}
-		hdr.Type = mt.MessageType(b)
+
+	// Shorthand and Message Type
+	if b, done, err := readByte(); err != nil {
+		return err
+	} else if done {
+		return errors.New("short read on byte 2 (composite of Shorthand and Type)")
+	} else {
+		hdr.Shorthand = (b & 0b10000000) == 1
+		hdr.Type = mt.MessageType(b & 0b01111111)
 	}
-	{ // Payload Length
-		MSB, done, err := readByte()
-		if done || err != nil {
-			return err
+
+	// ID (only if !shorthand)
+	if !hdr.Shorthand {
+		for i := range 8 {
+			if b, done, err := readByte(); err != nil {
+				return err
+			} else if done {
+				return fmt.Errorf("short read on byte %d (within ID bytes)", i+2)
+			} else {
+				hdr.ID = hdr.ID | (uint64(b) << ((7 - i) * 8))
+			}
 		}
-		LSB, done, err := readByte()
-		if done || err != nil {
-			return err
-		}
-		hdr.PayloadLength = uint16(MSB)<<8 | uint16(LSB)
+
 	}
 
 	return nil
@@ -129,29 +153,31 @@ func (hdr *Header) Deserialize(rd io.Reader) (err error) {
 
 // Validate tests each field in header, returning a list of issues.
 func (hdr *Header) Validate() (errors []error) {
+	// Version
 	if hdr.Version.Major > 15 {
 		errors = append(errors, ErrInvalidVersionMajor)
 	}
 	if hdr.Version.Minor > 15 {
 		errors = append(errors, ErrInvalidVersionMinor)
 	}
-	if hdr.PayloadLength > (math.MaxUint16 - uint16(FixedHeaderLen)) {
-		errors = append(errors, ErrInvalidPayloadLength)
+	// Type
+	if hdr.Type > 127 || hdr.Type == 0 { // type only has 7 bits available
+		errors = append(errors, ErrInvalidMessageType)
 	}
-	if hdr.Type == mt.UNKNOWN || hdr.Type.String() == "UNKNOWN" {
-		errors = append(errors, ErrUnknownMessageType)
+	if hdr.Shorthand && hdr.ID != 0 {
+		errors = append(errors, ErrShorthandID)
 	}
 
 	return errors
 }
 
 // Zerolog attaches header's fields to the given log event.
-// The given event is returned so it can be chained.
-func (hdr *Header) Zerolog(ev *zerolog.Event) *zerolog.Event {
+// Intended to be given to *zerolog.Event.Func().
+func (hdr *Header) Zerolog(ev *zerolog.Event) {
 	ev.Str("version", fmt.Sprintf("%d.%d", hdr.Version.Major, hdr.Version.Minor)).
-		Uint16("payload length", hdr.PayloadLength).
+		Bool("shorthand", hdr.Shorthand).
 		Str("type", hdr.Type.String())
-	return ev
+	if !hdr.Shorthand {
+		ev.Uint64("ID", hdr.ID)
+	}
 }
-
-//#region MessageType
