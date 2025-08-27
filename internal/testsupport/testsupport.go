12a417fc4daf8d4e546637bfe22c4eafb7841303
R Landau <rflandau@pm.me>
2025-08-26 18:31:29 -0700

    make ExpectedActual typesafe

--- internal/testsupport/testsupport.go
+++ internal/testsupport/testsupport.go
@@ -13,7 +13,7 @@ import (
 
 // ExpectedActual returns a newline-prefixed string comparing the expected result to the actual result.
 // Should be used to add clarity to unit test error messages.
-func ExpectedActual(expected, actual any) string {
+func ExpectedActual[T any](expected, actual T) string {
 	return fmt.Sprintf("\n\tExpected: '%v'\n\tActual: '%v'", expected, actual)
 }
 
