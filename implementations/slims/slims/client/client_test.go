b525a41f53c27c9e0d3540dc5a069a91b1fd1a6c
rflandau <rflandau@pm.me>
2025-08-28 19:10:57 -0700

    reimplement status tests

--- implementations/slims/slims/client/client_test.go
+++ implementations/slims/slims/client/client_test.go
@@ -2,48 +2,75 @@ package client
 
 import (
 	"context"
+	"math/rand/v2"
 	"net/netip"
 	"slices"
 	"testing"
 	"time"
 
+	"github.com/rflandau/Orv/implementations/slims/slims"
 	"github.com/rflandau/Orv/implementations/slims/slims/protocol"
 	vaultkeeper "github.com/rflandau/Orv/implementations/slims/slims/vk"
 	. "github.com/rflandau/Orv/internal/testsupport"
 )
 
+// Tests Status by spinning up a couple VKs and ensuring Status returns the correct information.
 func TestStatus(t *testing.T) {
-	pingTimeout := 300 * time.Millisecond
+	reqTimeout := 300 * time.Millisecond
+	var vkIDA, vkIDB slims.NodeID = 1, rand.Uint64()
 
 	// Spawn a VK to hit
-	vk, err := vaultkeeper.New(1, netip.MustParseAddrPort("[::0]:8081"))
+	vkA, err := vaultkeeper.New(vkIDA, netip.MustParseAddrPort("[::0]:8081"))
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	vk.Start()
-
-	// ensure we can ping the vk
-	if err := CoAPPing("[::0]:8081", pingTimeout); err != nil {
+	if err := vkA.Start(); err != nil {
 		t.Fatal(err)
 	}
 
-	// submit a status request
-	ctx, cancel := context.WithTimeout(t.Context(), pingTimeout)
+	// submit a shorthand status request
+	ctx, cancel := context.WithTimeout(t.Context(), reqTimeout)
 	defer cancel()
-	resp, err := Status("[::0]:8081", ctx)
+	respSR, err := Status(netip.MustParseAddrPort("[::0]:8081"), ctx)
 	if err != nil {
 		t.Fatal(err)
 	}
-	// validate the fields in resp
-	if resp.Height != 0 {
-		t.Error("bad height", ExpectedActual(0, resp.Height))
+	// validate the response
+	if vkA.Height() != uint16(respSR.Height) || respSR.Height != 0 {
+		t.Error("bad height", ExpectedActual(0, respSR.Height))
 	}
-	if resp.Id != 1 {
-		t.Error("bad vkID", ExpectedActual(1, resp.Id))
+	if vkA.ID() != respSR.Id || vkA.ID() != vkIDA {
+		t.Error("bad vkID", ExpectedActual(1, respSR.Id))
 	}
 	actualVersions := protocol.VersionsSupportedAsBytes()
-	if slices.Compare(actualVersions, resp.VersionsSupported) != 0 {
-		t.Error("mismatching version list", ExpectedActual(resp.VersionsSupported, actualVersions))
+	if slices.Compare(actualVersions, respSR.VersionsSupported) != 0 {
+		t.Error("mismatching version list", ExpectedActual(respSR.VersionsSupported, actualVersions))
+	}
+
+	// spawn a second VK with alternate values
+	vkB, err := vaultkeeper.New(vkIDB, netip.MustParseAddrPort("[::0]:8082"), vaultkeeper.WithDragonsHoard(3))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := vkB.Start(); err != nil {
+		t.Fatal(err)
+	}
+
+	// submit a longform status request
+	ctxB, cancelB := context.WithTimeout(t.Context(), reqTimeout)
+	defer cancelB()
+	respSR, err = Status(netip.MustParseAddrPort("[::0]:8082"), ctxB)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if vkB.Height() != uint16(respSR.Height) || respSR.Height != 3 {
+		t.Error("bad height", ExpectedActual(3, respSR.Height))
+	}
+	if vkB.ID() != respSR.Id || vkB.ID() != vkIDB {
+		t.Error("bad vkID", ExpectedActual(vkB.ID(), respSR.Id))
+	}
+	if slices.Compare(actualVersions, respSR.VersionsSupported) != 0 {
+		t.Error("mismatching version list", ExpectedActual(respSR.VersionsSupported, actualVersions))
 	}
 }
