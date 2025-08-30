3019168bc19a184e3a1896055750f87cf8849a10
R Landau <rflandau@pm.me>
2025-08-30 14:53:42 -0700

    remove old constant

--- implementations/slims/slims/orv.go
+++ implementations/slims/slims/orv.go
@@ -3,16 +3,8 @@
 // Child packages are mostly self-contained, the Orv parent package provides the few shared utilities.
 package slims
 
-import (
-	"github.com/plgd-dev/go-coap/v3/message"
-)
-
 // NodeID is the VK or Leaf's unique identifier
 type NodeID = uint64
-
-// ResponseMediaType returns the media type Orv uses by default.
-// Getter function as we cannot const a custom type (even if it is a primitive).
-func ResponseMediaType() message.MediaType { return message.AppOctets }
 
 // MaxPacketSize specifies the buffer size used to hold UDP payloads.
 // UDP can theoretically support payloads nearing 65535 bytes, but 1500B is a common MTU and Slims assumes that each Orv message fits into a single packet.
