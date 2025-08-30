a93dbdb3a90fd8e274c328e74e7520133524390a
R Landau <rflandau@pm.me>
2025-08-30 10:30:47 -0700

    update errors

--- implementations/slims/slims/vk/errors.go
+++ implementations/slims/slims/vk/errors.go
@@ -4,11 +4,20 @@ import (
 	"errors"
 	"fmt"
 	"net/netip"
+
+	"github.com/rflandau/Orv/implementations/slims/slims/protocol/mt"
 )
 
-// invalid addrport
+// ErrBadAddr returns an error to indicate that the given netip.AddrPort was invalid
 func ErrBadAddr(ap netip.AddrPort) error {
 	return fmt.Errorf("address %v is not a valid ip:port", ap)
 }
 
-var ErrDead = errors.New("this VaultKeeper is dead")
+// ErrBodyNotAccepted returns an error to indicated that a body was included in a packet type that does not accept a body
+func ErrBodyNotAccepted(typ mt.MessageType) error {
+	return errors.New("packet type " + typ.String() + " should not contain a body")
+}
+
+var (
+	ErrDead = errors.New("this VaultKeeper is dead")
+)
