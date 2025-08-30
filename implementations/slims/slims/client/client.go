6ebb64b94c1a85ef5ef1d81d3dafd8abf69def07
R Landau <rflandau@pm.me>
2025-08-30 15:27:33 -0700

    first pass Hello

--- implementations/slims/slims/client/client.go
+++ implementations/slims/slims/client/client.go
@@ -1,5 +1,5 @@
 // Package client provides static subroutines for interacting with a vault and vaultkeeper.
-// These subroutines can be called from any node (vk, leaf, or external)
+// Each subroutine lists if it is intended to be used only by leaves, only by vks, or by any node (even those not associated to the tree).
 package client
 
 import (
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"net"
 	"net/netip"
+	"time"
 
 	"github.com/rflandau/Orv/implementations/slims/slims"
 	"github.com/rflandau/Orv/implementations/slims/slims/pb"
@@ -16,106 +17,76 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
+var ErrInvalidAddrPort = errors.New("target must be a valid address+port")
+
 // Hello sends a HELLO packet to the given address, returning the target node's response or an error.
-func Hello(id slims.NodeID, addrPort string, ctx context.Context) (_ *pb.HelloAck, err error) {
-	/*
-	   // compose the body
-	   body, err := proto.Marshal(&pb.Hello{Id: id})
+// Sends the packet as protocol.SupportedVersions().HighestSupported().
+//
+// This subroutine can be invoked by any node.
+func Hello(id slims.NodeID, target netip.AddrPort, ctx context.Context) (vkID slims.NodeID, _ *pb.HelloAck, err error) {
+	// validate parameters
+	if ctx == nil {
+		return 0, nil, slims.ErrCtxIsNil
+	} else if !target.IsValid() {
+		return 0, nil, ErrInvalidAddrPort
+	}
+	UDPAddr := net.UDPAddrFromAddrPort(target)
+	if UDPAddr == nil {
+		return 0, nil, ErrInvalidAddrPort
+	}
 
-	   	if err != nil {
-	   		return nil, err
-	   	}
-
-	   // compose the header
-
-	   	reqHdr := protocol.Header{
-	   		Version: protocol.HighestSupported,
-	   		Type:    mt.Hello,
-	   	}
-
-	   reqHdrB, err := reqHdr.Serialize()
-
-	   	if err != nil {
-	   		return nil, err
-	   	}
-
-	   conn, err := udp.Dial(addrPort)
-
-	   	if err != nil {
-	   		return nil, err
-	   	}
-
-	   defer conn.Close()
-
-	   resp, err := conn.Post(ctx, "/", ResponseMediaType(), bytes.NewReader(append(reqHdrB, body...)))
-	   var hdrBytes = make([]byte, protocol.LongHeaderLen)
-
-	   	if n, err := resp.Body().Read(hdrBytes); err != nil {
-	   		return nil, err
-	   	} else if n != int(protocol.LongHeaderLen) {
-
-	   		return nil, fmt.Errorf("incorrect read count (%d, expected %d)", n, protocol.LongHeaderLen)
-	   	}
-
-	   // read the header
-	   respHeader := protocol.Header{}
-
-	   	if err := respHeader.Deserialize(bytes.NewBuffer(hdrBytes)); err != nil {
-	   		return nil, err
-	   	}
-
-	   // read the payload
-	   var respBody bytes.Buffer
-
-	   	if _, err := io.Copy(&respBody, resp.Body()); err != nil {
-	   		return nil, err
-	   	}
-
-	   // if the packet type is FAULT, unmarshal as a fault
-	   switch respHeader.Type {
-	   case mt.Fault:
-
-	   	// fetch the reason
-	   	var f pb.Fault
-	   	if err := proto.Unmarshal(respBody.Bytes(), &f); err != nil {
-	   		return nil, err
-	   	}
-	   	f.Reason = strings.TrimSpace(f.Reason)
-	   	if f.Reason == "" {
-	   		return nil, errors.New("FAULT occurred, but no reason was given")
-	   	}
-	   	return nil, errors.New(f.Reason)
-
-	   case mt.HelloAck:
-
-	   		var r pb.HelloAck
-	   		if err := proto.Unmarshal(respBody.Bytes(), &r); err != nil {
-	   			return nil, err
-	   		}
-	   		return &pb.HelloAck{
-	   			Id:      r.Id,
-	   			Height:  r.Height,
-	   			Version: r.Version,
-	   		}, nil
-	   	}
-
-	   return nil, errors.New("unexpected response message type: " + respHeader.Type.String())
-	*/
-	// TODO
-	return nil, nil
+	// generate a packet
+	pkt, err := protocol.Serialize(protocol.SupportedVersions().HighestSupported(), false, mt.Hello, id, nil)
+	if err != nil {
+		return 0, nil, err
+	}
+	// generate a dialer
+	conn, err := net.DialUDP("udp", nil, UDPAddr)
+	if err != nil {
+		return 0, nil, err
+	}
+	// send
+	if wroteN, err := ctxWrite(ctx, conn, pkt); err != nil {
+		return 0, nil, err
+	} else if len(pkt) != wroteN {
+		return 0, nil, fmt.Errorf("unexpected byte count written to target. Expected %dB, wrote %dB", len(pkt), wroteN)
+	}
+	// receive
+	_, _, respHdr, respBody, err := protocol.ReceivePacket(conn, ctx)
+	if err != nil {
+		return 0, nil, err
+	}
+	switch respHdr.Type {
+	case mt.Fault:
+		f := &pb.Fault{}
+		if err := proto.Unmarshal(respBody, f); err != nil {
+			return respHdr.ID, nil, err
+		}
+		return respHdr.ID, nil, errors.New(f.Reason)
+	case mt.HelloAck:
+		ha := &pb.HelloAck{}
+		if err := proto.Unmarshal(respBody, ha); err != nil {
+			return respHdr.ID, nil, err
+		}
+		return respHdr.ID, ha, nil
+	default:
+		return respHdr.ID, nil, fmt.Errorf("unhandled message type from response: %s", respHdr.Type.String())
+	}
 }
 
 // Status sends a STATUS packet to the given address and returns its answer (or an error).
 // ID is optional; if given, the STATUS packet will be sent long-form.
 // If omitted, the STATUS packet will be sent shorthand.
+//
+// This subroutine can be invoked by any node.
 func Status(target netip.AddrPort, ctx context.Context, senderID ...slims.NodeID) (vkID slims.NodeID, _ *pb.StatusResp, _ error) {
 	var sr *pb.StatusResp
 	if !target.IsValid() {
-		return 0, sr, errors.New("ap must be a valid address+port")
+		return 0, sr, ErrInvalidAddrPort
 	}
 	UDPAddr := net.UDPAddrFromAddrPort(target)
 	if UDPAddr == nil {
-		return 0, sr, errors.New("ap must be a valid address+port")
+		return 0, sr, ErrInvalidAddrPort
 	}
 
 	// generate a header
@@ -164,5 +135,39 @@ func Status(target netip.AddrPort, ctx context.Context, senderID ...slims.NodeID
 		return respHdr.ID, sr, nil
 	default:
 		return respHdr.ID, sr, fmt.Errorf("unhandled message type from response: %s", respHdr.Type.String())
+	}
+}
+
+// ctxWrite writes a bytestream over the given connection, but also watches for context cancellation to return early.
+func ctxWrite(ctx context.Context, conn *net.UDPConn, b []byte) (int, error) {
+	if ctx == nil {
+		return 0, slims.ErrCtxIsNil
+	}
+	// clear out any existing deadline and ensure we do the same on exist
+	if err := conn.SetWriteDeadline(time.Time{}); err != nil {
+		return 0, err
+	}
+	defer conn.SetWriteDeadline(time.Time{})
+
+	// spin up a channel to receive the write results
+	resCh := make(chan struct {
+		n   int
+		err error
+	}, 1) // buffer the channel so we do not leak the goroutine
+	go func() {
+		n, err := conn.Write(b)
+		resCh <- struct {
+			n   int
+			err error
+		}{n, err}
+		close(resCh)
+	}()
+
+	select {
+	case <-ctx.Done(): //handle if the context is cancelled
+		conn.SetWriteDeadline(time.Now())
+		return 0, ctx.Err()
+	case res := <-resCh:
+		return res.n, res.err
 	}
 }
