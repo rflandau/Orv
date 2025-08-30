5e0f01e877cf98ef9a4a2fc0cd64ad6b0e65fcc6
R Landau <rflandau@pm.me>
2025-08-30 11:28:28 -0700

    -count=1

--- implementations/slims/magefiles/magefile.go
+++ implementations/slims/magefiles/magefile.go
@@ -33,6 +33,6 @@ func Build() error {
 // Runs all Slims tests.
 // Tests are run with -race.
 func Test() error {
-	_, err := sh.Exec(nil, os.Stdout, os.Stderr, "go", "test", "./...", "-race")
+	_, err := sh.Exec(nil, os.Stdout, os.Stderr, "go", "test", "./...", "-race", "-count=1")
 	return err
 }
