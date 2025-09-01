c43900adfd8402e746bc4bf263304ad00a8145db
R Landau <rflandau@pm.me>
2025-09-01 14:34:30 -0700

    update RandomPort to only return valid ports at or above 1024

--- implementations/slims/internal/misc/misc.go
+++ implementations/slims/internal/misc/misc.go
@@ -1,3 +1,4 @@
+// Package misc is a placeholder package for internal utilities that are shared across packages, but do not have any shared characteristics.
 package misc
 
 import (
@@ -5,6 +6,7 @@ import (
 	"math/rand/v2"
 )
 
+// RandomPort returns a random port number from 1024 - 65535
 func RandomPort() uint16 {
-	return uint16(rand.Uint32N(math.MaxUint16))
+	return uint16(1024 + rand.Uint32N((math.MaxUint16 - 1024)))
 }
