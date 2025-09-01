2a6d242b1eb1c9e9136fef8c181e34952b6f20b4
R Landau <rflandau@pm.me>
2025-09-01 14:35:02 -0700

    upgrade tests to check for cvk

--- implementations/slims/slims/vk/children_test.go
+++ implementations/slims/slims/vk/children_test.go
@@ -16,40 +16,61 @@ func Test_addLeaf(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	// add a new leaf
-	leafID := rand.Uint64()
-	if isCVK := vk.addLeaf(leafID); isCVK {
-		t.Fatal("conflicted with existing cvk, despite being only child")
-	}
-	// ensure the leaf exists
-	if leaf, found := vk.children.leaves[leafID]; !found {
-		t.Error("failed to find leaf after insertion")
-	} else if !leaf.servicelessPruner.Stop() {
-		t.Error("serviceless pruner had already expired or been stopped")
-	}
-	// add the same leaf and check again
-	if isCVK := vk.addLeaf(leafID); isCVK {
-		t.Fatal("conflicted with existing cvk, despite being only child")
-	}
-	if leaf, found := vk.children.leaves[leafID]; !found {
-		t.Error("failed to find leaf after insertion")
-	} else if leaf.servicelessPruner.Stop() {
-		t.Error("serviceless pruner had not been stopped already despite explicit stop call")
-	}
-	// add another new leaf
-	leafID = rand.Uint64()
-	if isCVK := vk.addLeaf(leafID); isCVK {
-		t.Fatal("conflicted with existing cvk, despite being only child")
-	}
-	// ensure the leaf exists
-	if _, found := vk.children.leaves[leafID]; !found {
-		t.Error("failed to find leaf after insertion")
-	}
-	// let the leaf expire
-	time.Sleep(41 * time.Millisecond)
-	// ensure the leaf does not exist
-	if _, found := vk.children.leaves[leafID]; found {
-		t.Error("leaf should have expired")
+
+	{ // add a new leaf
+		leafID := rand.Uint64()
+		if isCVK := vk.addLeaf(leafID); isCVK {
+			t.Fatal("conflicted with existing cvk, despite being only child")
+		}
+		// ensure the leaf exists
+		if leaf, found := vk.children.leaves[leafID]; !found {
+			t.Error("failed to find leaf after insertion")
+		} else if !leaf.servicelessPruner.Stop() {
+			t.Error("serviceless pruner had already expired or been stopped")
+		}
+		// add the same leaf and check again
+		if isCVK := vk.addLeaf(leafID); isCVK {
+			t.Fatal("conflicted with existing cvk, despite being only child")
+		}
+		if leaf, found := vk.children.leaves[leafID]; !found {
+			t.Error("failed to find leaf after insertion")
+		} else if leaf.servicelessPruner.Stop() {
+			t.Error("serviceless pruner had not been stopped already despite explicit stop call")
+		}
 	}
 
+	{ // add another new leaf
+		leafID := rand.Uint64()
+		if isCVK := vk.addLeaf(leafID); isCVK {
+			t.Fatal("conflicted with existing cvk, but no cvks should exist")
+		}
+		// ensure the leaf exists
+		if _, found := vk.children.leaves[leafID]; !found {
+			t.Error("failed to find leaf after insertion")
+		}
+		// let the leaf expire
+		time.Sleep(41 * time.Millisecond)
+		// ensure the leaf does not exist
+		if _, found := vk.children.leaves[leafID]; found {
+			t.Error("leaf should have expired")
+		}
+	}
+	// add a cvk, then add a leaf with the same id
+	{
+		nodeID := rand.Uint64()
+		if isLeaf := vk.addCVK(nodeID, netip.MustParseAddrPort("127.0.0.1:1")); isLeaf {
+			t.Fatal("conflicted with existing leaf")
+		}
+		// ensure the cvk exists
+		if _, found := vk.children.cvks.Load(nodeID); !found {
+			t.Error("failed to find cvk after insertion")
+		}
+		// attempt to install a leaf with the same id
+		if isCVK := vk.addLeaf(nodeID); !isCVK {
+			t.Error("addLeaf should have recognized that a cvk with the same id already exists")
+		}
+		if _, found := vk.children.leaves[nodeID]; found {
+			t.Error("leave was created despite conflicted cvk id")
+		}
+	}
 }
