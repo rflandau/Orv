de3d1c551a0c24eaa2160d35cfadc9e51b17f9c7
R Landau <rflandau@pm.me>
2025-09-07 15:04:02 -0700

    clean out unused function

--- implementations/slims/internal/testsupport/testsupport.go
+++ implementations/slims/internal/testsupport/testsupport.go
@@ -2,13 +2,9 @@
 package testsupport
 
 import (
-	"context"
 	"fmt"
 	"maps"
 	"sync"
-	"time"
-
-	"github.com/plgd-dev/go-coap/v3/udp"
 )
 
 // ExpectedActual returns a newline-prefixed string comparing the expected result to the actual result.
@@ -42,21 +38,4 @@ func SlicesUnorderedEqual[T comparable](a []T, b []T) bool {
 
 	wg.Wait()
 	return maps.Equal(am, bm)
-}
-
-// CoAPPing is a helper function that sends a ping to the given address.
-func CoAPPing(addr string, timeout time.Duration) error {
-	// TODO remove
-	// ping the server
-	conn, err := udp.Dial(addr)
-	if err != nil {
-		return err
-	}
-	defer conn.Close()
-	ctx, cancel := context.WithTimeout(context.Background(), timeout)
-	defer cancel()
-	if err := conn.Ping(ctx); err != nil {
-		return err
-	}
-	return nil
 }
