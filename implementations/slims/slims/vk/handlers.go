9638e6bd21aefddcd4fc7a122bc24f268b8ae3a1
R Landau <rflandau@pm.me>
2025-09-21 09:13:07 -0700

    WIP update service handlers to use errno

--- implementations/slims/slims/vk/handlers.go
+++ implementations/slims/slims/vk/handlers.go
@@ -14,7 +14,6 @@ import (
 	"github.com/rflandau/Orv/implementations/slims/slims/client"
 	"github.com/rflandau/Orv/implementations/slims/slims/pb"
 	"github.com/rflandau/Orv/implementations/slims/slims/protocol"
-	"github.com/rflandau/Orv/implementations/slims/slims/protocol/mt"
 	"google.golang.org/protobuf/proto"
 )
 
@@ -26,14 +25,13 @@ const registerPropagateTimeout time.Duration = 1 * time.Second
 
 // serveStatus answers STATUS packets, returning a variety of information about the vk.
 // Briefly holds a read lock on structure.
-func (vk *VaultKeeper) serveStatus(_ protocol.Header, reqBody []byte, senderAddr net.Addr) {
+func (vk *VaultKeeper) serveStatus(_ protocol.Header, reqBody []byte, senderAddr net.Addr) (errored bool, errno pb.Fault_Errnos, extraInfo []string) {
 	// no header validation is required
 
 	// check that we were not given a body
 	if len(reqBody) != 0 {
 		vk.log.Warn().Int("body length", len(reqBody)).Str("body", string(bytes.TrimSpace(reqBody))).Msg("STATUS message has body")
-		vk.respondError(senderAddr, ErrBodyNotAccepted(mt.Status).Error())
-		return
+		return true, pb.Fault_BODY_NOT_ACCEPTED, nil
 	}
 
 	vk.structure.mu.RLock()
@@ -46,21 +44,20 @@ func (vk *VaultKeeper) serveStatus(_ protocol.Header, reqBody []byte, senderAddr
 
 	// send data to client
 	vk.respondSuccess(senderAddr,
-		protocol.Header{Version: vk.versionSet.HighestSupported(), Type: mt.StatusResp, ID: vk.id},
+		protocol.Header{Version: vk.versionSet.HighestSupported(), Type: pb.MessageType_STATUS_RESP, ID: vk.id},
 		st)
+	return
 }
 
 // serveHello answers HELLO packets with HELLO_ACK or FAULT.
 // Selects a version based on the request version (the version in the header) and what versions we support.
-func (vk *VaultKeeper) serveHello(reqHdr protocol.Header, reqBody []byte, senderAddr net.Addr) {
+func (vk *VaultKeeper) serveHello(reqHdr protocol.Header, reqBody []byte, senderAddr net.Addr) (errored bool, errno pb.Fault_Errnos, extraInfo []string) {
 	// validate parameters
 	if reqHdr.Shorthand {
-		vk.respondError(senderAddr, ErrShorthandNotAccepted(mt.Hello).Error())
-		return
+		return true, pb.Fault_SHORTHAND_NOT_ACCEPTED, nil
 	} else if len(reqBody) != 0 {
 		vk.log.Warn().Int("body length", len(reqBody)).Str("body", string(bytes.TrimSpace(reqBody))).Msg("HELLO message has body")
-		vk.respondError(senderAddr, ErrBodyNotAccepted(mt.Hello).Error())
-		return
+		return true, pb.Fault_BODY_NOT_ACCEPTED, nil
 	} // no need to check version here as we would normally reply according to packet rules, but we only support a single version
 
 	// install requestor id in our pending table
@@ -69,124 +66,109 @@ func (vk *VaultKeeper) serveHello(reqHdr protocol.Header, reqBody []byte, sender
 	vk.respondSuccess(senderAddr,
 		protocol.Header{
 			Version: vk.versionSet.HighestSupported(), // at the moment, we only support a single version, so only reply with that version
-			Type:    mt.HelloAck,
+			Type:    pb.MessageType_HELLO_ACK,
 			ID:      vk.ID()},
 		&pb.HelloAck{Height: uint32(vk.Height())})
+
+	return
 }
 
 // serveJoin answers JOIN packets with JOIN_ACCEPT or FAULT, installing the requestor as a child iff their height is appropriate.
-func (vk *VaultKeeper) serveJoin(reqHdr protocol.Header, reqBody []byte, senderAddr net.Addr) {
+func (vk *VaultKeeper) serveJoin(reqHdr protocol.Header, reqBody []byte, senderAddr net.Addr) (errored bool, errno pb.Fault_Errnos, extraInfo []string) {
 	if reqHdr.Shorthand {
-		vk.respondError(senderAddr, ErrShorthandNotAccepted(mt.Join).Error())
-		return
+		return true, pb.Fault_SHORTHAND_NOT_ACCEPTED, nil
 	} else if len(reqBody) == 0 {
-		vk.respondError(senderAddr, ErrBodyRequired(mt.Join).Error())
-		return
+		return true, pb.Fault_BODY_REQUIRED, nil
 	} else if !vk.versionSet.Supports(reqHdr.Version) {
-		vk.respondError(senderAddr, ErrVersionNotSupported(reqHdr.Version).Error())
-		return
+		return true, pb.Fault_VERSION_NOT_SUPPORTED, nil
 	}
 
 	// unpack the body
 	var j = &pb.Join{}
-	if err := proto.Unmarshal(reqBody, j); err != nil {
-		vk.respondError(senderAddr, ErrFailedToUnmarshal(mt.Join, err).Error())
-		return
-	}
+	if err := pbun.Unmarshal(reqBody, j); err != nil {
+		return true, pb.Fault_MALFORMED_BODY, nil
+	} // TODO check for unknown fields
 
 	// acquire lock
 	vk.structure.mu.Lock()
 	defer vk.structure.mu.Unlock()
 
 	// handle as leaf or as cvk
-	if j.IsVK {
+	if j.IsVk {
 		// validate rest of body
 		if j.Height != uint32(vk.structure.height)-1 {
-			vk.respondError(senderAddr, ErrBadHeightJoin(vk.structure.height, uint16(j.Height)).Error())
-			return
+			return true, pb.Fault_BAD_HEIGHT, []string{fmt.Sprintf("to join as a child VK to this VK, height (%d) must equal parent VK height (%d)-1", j.Height, vk.Height())}
 		}
 		addr, err := netip.ParseAddrPort(j.VkAddr)
 		if err != nil {
-			vk.respondError(senderAddr, ErrBadAddr(addr).Error())
-			return
+			return true, pb.Fault_MALFORMED_ADDRESS, nil
 		}
 		// check if we can refresh a node with this ID
 		if !vk.children.cvks.Refresh(reqHdr.ID, vk.pruneTime.ChildVK) {
 			// ensure that the requestor is in our hello table
 			if found := vk.pendingHellos.Delete(reqHdr.ID); !found {
-				vk.respondError(senderAddr, "must send HELLO first and join before it expires")
-				return
+				return true, pb.Fault_HELLO_REQUIRED, nil
 			}
 			// install the new child
 			if vk.addCVK(reqHdr.ID, addr) {
-				vk.respondError(senderAddr, fmt.Sprintf("ID %d is already in use by a leaf", reqHdr.ID))
-				return
+				return true, pb.Fault_ID_IN_USE, nil
 			}
 		}
 
 	} else {
 		// ensure that the requestor is in our hello table
 		if found := vk.pendingHellos.Delete(reqHdr.ID); !found {
-			vk.respondError(senderAddr, "must send HELLO first and join before it expires")
-			return
+			return true, pb.Fault_HELLO_REQUIRED, nil
 		}
 		if isCVK := vk.addLeaf(reqHdr.ID); isCVK {
-			vk.respondError(senderAddr, fmt.Sprintf("ID %d is already in use by child vk", reqHdr.ID))
-			return
+			return true, pb.Fault_ID_IN_USE, nil
 		}
 	}
 
-	vk.log.Info().Uint64("child ID", reqHdr.ID).Str("child VK address", j.VkAddr).Bool("vk?", j.IsVK).Msg("accepted JOIN")
+	vk.log.Info().Uint64("child ID", reqHdr.ID).Str("child VK address", j.VkAddr).Bool("vk?", j.IsVk).Msg("accepted JOIN")
 
 	vk.respondSuccess(senderAddr,
 		protocol.Header{Version: vk.versionSet.HighestSupported(),
-			Type: mt.JoinAccept,
+			Type: pb.MessageType_JOIN_ACCEPT,
 			ID:   vk.ID(),
 		}, &pb.JoinAccept{Height: uint32(vk.structure.height)})
 
+	return
 }
 
-func (vk *VaultKeeper) serveRegister(reqHdr protocol.Header, reqBody []byte, senderAddr net.Addr) {
+func (vk *VaultKeeper) serveRegister(reqHdr protocol.Header, reqBody []byte, senderAddr net.Addr) (errored bool, errno pb.Fault_Errnos, extraInfo []string) {
 	// validate parameters
 	if reqHdr.Shorthand {
-		vk.respondError(senderAddr, ErrShorthandNotAccepted(mt.Register).Error())
-		return
+		return true, pb.Fault_SHORTHAND_NOT_ACCEPTED, nil
 	} else if len(reqBody) == 0 {
-		vk.respondError(senderAddr, ErrBodyRequired(mt.Register).Error())
-		return
+		return true, pb.Fault_BODY_REQUIRED, nil
 	} else if !vk.versionSet.Supports(reqHdr.Version) {
-		vk.respondError(senderAddr, ErrVersionNotSupported(reqHdr.Version).Error())
-		return
+		return true, pb.Fault_VERSION_NOT_SUPPORTED, nil
 	}
 	// parse and validate body
 	var registerReq pb.Register
 	if err := proto.Unmarshal(reqBody, &registerReq); err != nil {
-		vk.respondError(senderAddr, ErrFailedToUnmarshal(mt.Register, err).Error())
-		return
+		return true, pb.Fault_MALFORMED_BODY, nil
 	} else if registerReq.Service == "" {
-		vk.respondError(senderAddr, "service name cannot be empty")
-		return
+		return true, pb.Fault_BAD_SERVICE_NAME, []string{"service name cannot be empty"}
 	}
 	serviceStaleTime, err := time.ParseDuration(registerReq.Stale)
 	if err != nil {
-		vk.respondError(senderAddr, "bad stale time: "+err.Error())
-		return
+		return true, pb.Fault_BAD_STALE_TIME, nil
 	}
 	serviceAddress, err := netip.ParseAddrPort(registerReq.Address)
 	if err != nil {
-		vk.respondError(senderAddr, "bad service address: "+err.Error())
-		return
+		return true, pb.Fault_MALFORMED_ADDRESS, []string{fmt.Sprintf("bad service address ('%s' @ )")}
 	}
 	// register the service
-	if err := vk.addService(reqHdr.ID, registerReq.Service, serviceAddress, serviceStaleTime); err != nil {
-		vk.respondError(senderAddr, err.Error())
-		return
+	if erred, code, ei := vk.addService(reqHdr.ID, registerReq.Service, serviceAddress, serviceStaleTime); err != nil {
+		return erred, code, ei
 	}
 
 	// respond with acceptance
 	vk.respondSuccess(senderAddr, protocol.Header{
 		Version: vk.versionSet.HighestSupported(),
-		Type:    mt.RegisterAccept,
+		Type:    pb.MessageType_REGISTER_ACCEPT,
 		ID:      vk.id,
 	}, &pb.RegisterAccept{Service: registerReq.Service})
 
@@ -208,6 +190,7 @@ func (vk *VaultKeeper) serveRegister(reqHdr protocol.Header, reqBody []byte, sen
 		vk.log.Debug().AnErr("register error", err).Str("service", registerReq.Service).Str("service address", serviceAddress.String()).Msg("registered child service to parent")
 	}
 	vk.structure.mu.Unlock()
+	return
 }
 
 // serveServiceHeartbeat answers SERVICE_HEARTBEATs, refreshing all attached (known) services associated to the requestor's ID.
