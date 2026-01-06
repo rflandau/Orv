c7de3ec6955e3ea85b0eb7f0935bfab575f06134
rflandau <rflandau@pm.me>
2026-01-05 17:54:31 -0800

    add tests

--- implementations/slims/slims/slims_test.go
+++ implementations/slims/slims/slims_test.go
@@ -32,3 +32,48 @@ func TestErrno_Is(t *testing.T) {
 	}
 
 }
+
+func TestFormatFault(t *testing.T) {
+	t.Run("nil param", func(t *testing.T) {
+		if err := slims.FormatFault(nil); err != nil {
+			t.Fatal("FormatFault returned non-nil error when given nil parameter")
+		}
+	})
+
+	t.Run("IsErrno_NoAdditionalInfo", func(t *testing.T) {
+		errno := pb.Fault_MALFORMED_BODY
+
+		// generate a test fault
+		f := &pb.Fault{
+			Original: pb.MessageType_DEREGISTER,
+			Errno:    errno,
+		}
+		// check that FormatFault correctly returns an Errno error
+		err := slims.FormatFault(f)
+		if err == nil {
+			t.Fatal("Returned a nil error")
+		} else if !errors.Is(err, slims.Errno{Num: errno}) {
+			t.Fatal("Returned error is not the correct errno type")
+		}
+	})
+	t.Run("IsErrno_WithAdditionalInfo", func(t *testing.T) {
+		errno := pb.Fault_UNSPECIFIED
+
+		// generate a test fault
+		ai := "an unspecified error occurred"
+		f := &pb.Fault{
+			Original:       pb.MessageType_UNKNOWN,
+			Errno:          errno,
+			AdditionalInfo: &ai,
+		}
+		// check that FormatFault correctly returns an Errno error
+		err := slims.FormatFault(f)
+		if err == nil {
+			t.Fatal("Returned a nil error")
+		} else if !errors.Is(err, slims.Errno{Num: errno}) {
+			t.Fatal("Returned error is not the correct errno type (sans AI)")
+		} else if !errors.Is(err, slims.Errno{Num: errno, AdditionalInfo: ai}) {
+			t.Fatal("Returned error is not the correct errno type (with AI)")
+		}
+	})
+}
